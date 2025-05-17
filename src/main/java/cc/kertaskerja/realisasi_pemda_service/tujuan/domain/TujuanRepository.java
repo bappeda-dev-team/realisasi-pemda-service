dca2f19513c0b76eb434ad92dddb434227daedb2
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-05-17 08:44:12 +0700

    base realisasi tujuan service

--- src/main/java/cc/kertaskerja/realisasi_pemda_service/tujuan/domain/TujuanRepository.java
+++ src/main/java/cc/kertaskerja/realisasi_pemda_service/tujuan/domain/TujuanRepository.java
@@ -2,7 +2,6 @@ package cc.kertaskerja.realisasi_pemda_service.tujuan.domain;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public interface TujuanRepository extends ReactiveCrudRepository<Tujuan, Long> {
     Flux<Tujuan> findAllByTahun(String tahun);
