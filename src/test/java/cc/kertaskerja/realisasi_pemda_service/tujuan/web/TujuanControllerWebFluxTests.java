0cf6134d0cfe5ac7166336edf11125da696eef5f
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-06-07 12:11:42 +0700

    tujuan controller test

---
+++ src/test/java/cc/kertaskerja/realisasi_pemda_service/tujuan/web/TujuanControllerWebFluxTests.java
@@ -0,0 +1,77 @@
+package cc.kertaskerja.realisasi_pemda_service.tujuan.web;
+
+import cc.kertaskerja.realisasi_pemda_service.realisasi.domain.JenisRealisasi;
+import cc.kertaskerja.realisasi_pemda_service.tujuan.domain.Tujuan;
+import cc.kertaskerja.realisasi_pemda_service.tujuan.domain.TujuanService;
+import cc.kertaskerja.realisasi_pemda_service.tujuan.domain.TujuanStatus;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@WebFluxTest(TujuanController.class)
+public class TujuanControllerWebFluxTests {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockitoBean
+    private TujuanService tujuanService;
+
+    @Test
+    void whenTujuanIdParamsExistsInByTahunShouldReturnRealisasiTujuanByTahunAndTujuanId() {
+        List<Tujuan> mockTujuans = List.of(
+                Tujuan.of("TUJ-123", "Test-Tujuan",
+                        "IND-TUJ-123", "Produk-A",
+                        100.0, 100.0, "%", "2025",
+                        JenisRealisasi.NAIK, TujuanStatus.UNCHECKED));
+        Mockito.when(tujuanService.getRealisasiTujuanByTahunAndTujuanId("2025", "TUJ-123"))
+                .thenReturn(Flux.fromIterable(mockTujuans));
+
+        webTestClient
+                .mutateWith(SecurityMockServerConfigurers.mockJwt().authorities(new SimpleGrantedAuthority("ROLE_ADMIN")))
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/tujuans/by-tahun/2025")
+                        .queryParam("tujuanId", "TUJ-123")
+                        .build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Tujuan.class)
+                .hasSize(1)
+                .contains(mockTujuans.get(0));
+    }
+
+    @Test
+    void whenNoTujuanIdShouldReutrnRealisasiTujuanByTahun() {
+        List<Tujuan> mockTujuans = List.of(
+                Tujuan.of("TUJ-123", "Test-Tujuan",
+                        "IND-TUJ-123", "Produk-A",
+                        100.0, 100.0, "%", "2025",
+                        JenisRealisasi.NAIK, TujuanStatus.UNCHECKED),
+                Tujuan.of("TUJ-123", "Test-Tujuan",
+                        "IND-TUJ-123", "Produk-A",
+                        100.0, 100.0, "%", "2026",
+                        JenisRealisasi.NAIK, TujuanStatus.UNCHECKED)
+                );
+        Mockito.when(tujuanService.getRealisasiTujuanByTahun("2025"))
+                .thenReturn(Flux.just(mockTujuans.get(0)));
+
+        webTestClient
+                .mutateWith(SecurityMockServerConfigurers.mockJwt()
+                        .authorities(new SimpleGrantedAuthority("ROLE_ADMIN")))
+                .get()
+                .uri("/tujuans/by-tahun/2025")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Tujuan.class)
+                .hasSize(1)
+                .contains(mockTujuans.get(0));
+    }
+}
