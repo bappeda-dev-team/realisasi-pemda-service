a00321f80f0786216cfb2c42a6a9b207911b92f9
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-07-21 20:16:39 +0700

    domain for realisasi opd - tujuan opd

---
+++ src/main/java/cc/kertaskerja/realisasi_opd_service/tujuan/domain/TujuanOpd.java
@@ -0,0 +1,55 @@
+package cc.kertaskerja.realisasi_opd_service.tujuan.domain;
+
+import cc.kertaskerja.realisasi_pemda_service.realisasi.domain.JenisRealisasi;
+import cc.kertaskerja.realisasi_pemda_service.tujuan.domain.TujuanStatus;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.Instant;
+
+@Table("tujuan_opd")
+public record TujuanOpd(
+        @Id Long id,
+
+        String tujuanId,
+        String tujuan,
+        String indikatorId,
+        String indikator,
+        String targetId,
+        String target,
+        Double realisasi,
+        String satuan,
+        String tahun,
+        JenisRealisasi jenisRealisasi,
+        String kodeOpd,
+        TujuanStatus status,
+
+        @CreatedDate Instant createdDate,
+        @LastModifiedDate Instant lastModifiedDate,
+
+        @Version int version
+) {
+    public static TujuanOpd of(
+            String tujuanId,
+            String tujuan,
+            String indikatorId,
+            String indikator,
+            String targetId,
+            String target,
+            Double realisasi,
+            String satuan,
+            String tahun,
+            JenisRealisasi jenisRealisasi,
+            String kodeOpd,
+            TujuanStatus status
+    ) {
+        return new TujuanOpd(null,
+                tujuanId, tujuan, indikatorId, indikator,
+                targetId, target, realisasi, satuan, tahun,
+                jenisRealisasi, kodeOpd, status,
+                null, null, 0);
+    }
+}
---
+++ src/main/resources/db/migration/V3__Create_Tujuan_Opd_Table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE tujuan_opd
+(
+    id                 BIGSERIAL PRIMARY KEY NOT NULL,
+    tujuan_id          varchar(255)          NOT NULL,
+    tujuan             varchar(255)          NOT NULL,
+    indikator_id       varchar(255)          NOT NULL,
+    indikator          varchar(255)          NOT NULL,
+    target_id          varchar(255)          NOT NULL UNIQUE,
+    target             double precision      NOT NULL,
+    realisasi          double precision      NOT NULL,
+    satuan             varchar(255)          NOT NULL,
+    tahun              varchar(10)           NOT NULL,
+    jenis_realisasi    varchar(255)          NOT NULL,
+    kode_opd           varchar(255)          NOT NULL,
+    status             varchar(255)          NOT NULL,
+    created_date       timestamp             NOT NULL,
+    last_modified_date timestamp             NOT NULL,
+    version            integer               NOT NULL
+)
